ae85ea9686a0a8b305f6914e7263c60859388559
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-10-15 14:14:49 +0100

    Popup added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import SuccessMessage from './components/SuccessMessage';
 
 function App() {
+
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  function toggleSuccess() {
+    setShowSuccess(true);
+    setTimeout(() => {setShowSuccess(false);}, 3000);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Form />
+      <Form onFormSubmit={toggleSuccess}/>
+      {showSuccess && <SuccessMessage />}
     </div>
   );
 }
